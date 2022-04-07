b7c763deb17b4942bb1c7ec5eeb2bd2cecd492f7
tolulopejanet <tolulopefaleyejanet@gmail.com>
2022-04-07 09:26:30 +0100

    waiting list

---
+++ app.js
@@ -0,0 +1,26 @@
+var emailError = document.getElementById('email-error');
+var submitError = document.getElementById('submit-error');
+
+function validateEmail(){
+    var email = document.getElementById('contact-email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Invalid Email!';
+        return false;
+    }
+    emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
+
+function validateForm(){
+    if(!validateEmail()){
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix error to join';
+        setTimeout(function(){submitError.style.display = 'none';}, 3000);
+        return false;
+    }
+}
